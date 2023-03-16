b1032f85ba801de16e41fa21f4e06f175ae848a7
OlivierMaria <olivier-maria@hotmail.com>
2023-03-16 21:24:41 +0100

    commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,6 +65,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:name, :description, :price, :categories)
+      params.require(:product).permit(:name, :description, :price, :categories, :image)
     end
 end
